d1b2a80f6bb89aad1449413fdeaa7018166b5b02
Dhaval Lad <dhavallad92@gmail.com>
2015-11-19 09:29:14 -0500

    Add MPI_Program2.c

---
+++ MPI_Program2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main (int argc, char *argv[]){
+	
+	int numTasks, rank, dest, source, iS, count, tag=1;
+	char inmsg, outmsg="x"';
+	MPI_Status Stat;
+	MPI_Init (&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numTasks)'
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank)
+	if(rank == 0){
+		dest = 1;
+		source = 1;
+		iS = MPI_Send(&outmsg,1,MPI_CHAR,dest,tag,MPI_COMM_WORLD);
+		iS = MPI_Recv(&inmsg,1,MPI_CHAR,source,tag,MPI_COMM_WORLD,&Stat);
+	}
+	
+	else if(rank == 1){
+		dest = 0;
+		source = 0;
+		iS = MPI_Recv(&inmsg,1,MPI_CHAR,source,tag,MPI_COMM_WORLD,&Stat);
+		iS = MPI_Send(&outmsg,1,MPI_CHAR,dest,tag,MPI_COMM_WORLD);
+			
+	}
+	
+	iS = MPI_Get_count(&Stat,MPI_CHAR,&count);
+	printf("Task %d recievved %d char(s) from the thak %d woth tag %d \n", rank,count,Stat.MPI_SOURCE,Stat.MPI_TAG);
+	
+	MPI_Finalize();
+}
+	
+
+
+d (&outmsg, 1, MPI CHAR, dest, tag, MPI COMM WORLD)
+	rc MPI Get count (&Stat, MPI CHAR, &count)
+	printf ("Task td: Received td char (s) from task td with tag td In",
+	rank, count, Stat. MPI SOURCE, Stat. MPI TAG)
+}
