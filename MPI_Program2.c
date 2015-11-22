2a9b8c0c494313d8240d976fb8b1e27c8f9a7417
Dhaval Lad <dhavallad92@gmail.com>
2015-11-21 23:10:03 -0500

    Files Updated

--- MPI_Program2.c
+++ MPI_Program2.c
@@ -5,11 +5,11 @@
 int main (int argc, char *argv[]){
 	
 	int numTasks, rank, dest, source, iS, count, tag=1;
-	char inmsg, outmsg="x"';
+	char inmsg, outmsg='x';
 	MPI_Status Stat;
 	MPI_Init (&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD,&numTasks)'
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank)
+	MPI_Comm_size(MPI_COMM_WORLD,&numTasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	if(rank == 0){
 		dest = 1;
 		source = 1;
@@ -26,7 +26,7 @@ int main (int argc, char *argv[]){
 	}
 	
 	iS = MPI_Get_count(&Stat,MPI_CHAR,&count);
-	printf("Task %d recievved %d char(s) from the thak %d woth tag %d \n", rank,count,Stat.MPI_SOURCE,Stat.MPI_TAG);
+	printf("Task %d received %d char(s) from the task %d with tag %d \n", rank,count,Stat.MPI_SOURCE,Stat.MPI_TAG);
 	
 	MPI_Finalize();
 }
--- MPI_Program3.c
+++ MPI_Program3.c
@@ -13,18 +13,19 @@ int main (int argc, char **argv){
 	int comm_size;
 	MPI_Comm_size(MPI_COMM_WORLD,&comm_size);
 	
-	if(rank == 0){
-		fprintf(strderr,"Must use two processes for this example\n");
+	if(comm_size != 2){
+		fprintf(stderr,"Must use two processes for this example\n");
 		MPI_Abort(MPI_COMM_WORLD,1);
 	}
 	
 	int rank;
-	
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	int numbersToSend;
 	if(rank == 0){
 		const int MAX_NUMBERS = 100;
 		int numbers[MAX_NUMBERS];
 		srand(time(NULL));
-		numbersToSend = (rand() / (float)RAND_MAX * MAX_NUMBERS;
+		numbersToSend = (rand() / (float)RAND_MAX * MAX_NUMBERS);
 		MPI_Send(numbers,numbersToSend,MPI_INT,1,SENDREV_NUMS_TAG,MPI_COMM_WORLD);
 		printf(") sent %d numbers to 1\n",numbersToSend);
 	}
