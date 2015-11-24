2ee930e30eb7ed0cc230dcc73cbde67a8b726561
Dhaval Lad <dhavallad92@gmail.com>
2015-11-24 09:38:01 -0500

    Program 6 Updated

--- MPI_P6.c
+++ MPI_P6.c
@@ -5,34 +5,35 @@
 #include "mpi.h"
 main (int argc, char *argv[]){
 	
-	int i,numtasks, rank, next, partner, buf [2], tag1=1, tag2=2;
+	int i,numtasks, rank, next, partnerId, buf [2], tag=1, tag2=2;
 	
 	MPI_Init (&argc,&argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Request reqs[numtasks];
-	MPI_Status stats[numtasks];
+	MPI_Request reqs[2];
+	MPI_Status stats[2];
 	
-	if (rank == 0){
-		printf("Number of task is %d\n",numtasks);
-	} 
+	if (rank == 0) printf("Number of task is %d\n",numtasks);
 	
-	if(rank < numtasks/2)
-	partner = numtasks/2+rank;
-	else
-	partner = rank - numtasks/2;
-	
-	for(i=0;i<numtasks;i++){
-		MPI_Isend (&i,1,MPI_INT,partner,tag1,MPI_COMM_WORLD,&reqs[i]);
-		MPI_Irecv(&i,1,MPI_INT,partner,tag1,MPI_COMM_WORLD,&reqs[i]);
+	if(rank < numtasks/2){
+		partnerId = numtasks/2+rank;
+		// printf("%d:%d",rank,partnerId);	
 	}
+	else{
+		partnerId = rank - numtasks/2;
+		// printf("%d:%d",rank,partnerId);
+	}
+	MPI_Irecv(&partnerId,1,MPI_INT,partnerId,tag,MPI_COMM_WORLD,&reqs[0]);
+	MPI_Isend(&rank,1,MPI_INT,partnerId,tag,MPI_COMM_WORLD,&reqs[1]);
 	//do some work
-	MPI_Waitall (4, reqs, stats);
-	for(i=0;i<1;i++){
-		// 2 sends
-		printf("Task %d: Received from task %d with tag %d \n",
-		rank, stats[i].MPI_SOURCE, stats[i].MPI_TAG);
-	}
+	MPI_Waitall (2, reqs, stats);
+
+	printf("Task#:%d is partner with Task#%d - Task(%d)->Task(%d)\n",rank,partnerId,rank,partnerId);
+	// for(i=0;i<1;i++){
+	// 2 sends
+	// printf("Task %d: Received from task %d with tag %d \n",
+	// rank, stats[i].MPI_SOURCE, stats[i].MPI_TAG);
+	// }
 	MPI_Finalize();
 }
